696a8315ee2e04bba9084aaba62acd0e807057af
Nymphinia <157540443+Nymphinia@users.noreply.github.com>
2024-06-05 22:15:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ client.on('ready', async () => {
     .setApplicationId('1142516638852321280')
     .setType('STREAMING')
     .setURL('https://twitch.tv/developer') //Must be a youtube video link 
-    .setState('☆₊˚⋆𝐋ɪʟᴰᴿᴬᴳᴼᴺ𝐁ᴀʙɪɪ ଦ🪷ଓ₊˚⋆')
+    .setState('☆₊˚⋆𝐘𝐨-𝐘𝐨 𝐌𝐚𝐬𝐭𝐞𝐫♡ଦ🪷ଓ₊˚⋆')
     .setName('Dragon Nymph')
-    .setDetails(`𝑙𝑖𝑔ℎ𝑡𝑛𝑖𝑛𝑔 𝑢𝑠𝑢𝑟𝑝𝑒𝑟`)
+    .setDetails(`𝘼𝙧𝙘𝙖𝙣𝙖: 𝙃𝙤𝙥𝙚✰`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1075473281022447796/1240220875652075600/IMG_1642.gif?ex=665e28f2&is=665cd772&hm=f0acb23d3e1d35a10be6580fdfff61606e3bba796eff75c7b615ff26b328fa21&') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1075473281022447796/1248005119887544330/IMG_5988.gif?ex=666216d7&is=6660c557&hm=158260c4aa807b48a1f3a5d2a6b75af9e7e627554de2c75c6d89d5583d0f73bf&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('˚₊⊹♡₊˚꒷꒦꒷') //Text when you hover the Large image
-    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1075473281022447796/1240220969927315527/IMG_1633.gif?ex=665e2909&is=665cd789&hm=c906d4798306e844e0c170a0f86ed78f7e1e4177978f91c0e79b8c87eb6aa205&') //You can put links in tenor or discord and etc.
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1075473281022447796/1248004960273301504/IMG_1976.gif?ex=666216b1&is=6660c531&hm=9e4e97f3cb936b2cbdee926f6e11292c1be85d2502a6af7cbadd915006de97bd&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('𝑁𝑎𝑒𝑎𝑟𝑎♡') //Text when you hover the Small image
     .addButton('ೣ🌷 ໑ ִ 💗🎐','https://nymphibia.carrd.co/')
     .addButton('★☄🀄️💌','https://youtu.be/ArmDp-zijuc');

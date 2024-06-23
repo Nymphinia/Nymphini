d674ae637b8c24a1451abeb2ef342adb2aa8ca87
Nymphinia <157540443+Nymphinia@users.noreply.github.com>
2024-06-23 23:46:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ client.on('ready', async () => {
     .setName('Dragon Nymph')
     .setDetails(`❦𝙧𝙖𝙢𝙡𝙚𝙩𝙝𝙖𝙡☾✰`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1075473281022447796/1254373493127254047/ezgif-5-76b67b7a4c.gif?ex=667941d9&is=6677f059&hm=e710ee6fafe8822244eb398dedc2282add94a0427e703da1448b1ab564d03b7f&') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1075473281022447796/1254553173465632868/ezgif-2-68a6dc7b1e.gif?ex=6679e931&is=667897b1&hm=6aad424c102ef020e013f0f05f629bd0c2b0643de00fcee5e586f3857679431a&') //You can put links in tenor or discord and etc.
     .setAssetsLargeText('˚₊⊹♡₊˚꒷꒦꒷') //Text when you hover the Large image
     .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1075473281022447796/1254336325155557436/ezgif-2-39c8da1469-unscreen.gif?ex=66791f3c&is=6677cdbc&hm=d514a8f863f7147246b70104f1b1b8977c09fc9a0d1854486ca26c738490a259&') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('𝑁𝑎𝑒𝑎𝑟𝑎♡') //Text when you hover the Small image
